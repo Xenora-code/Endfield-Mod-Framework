aa9a1fe56d3ecba33b52fe67bf6611ee088fdeb4
Xenora-code <nerozero.us@gmail.com>
2026-02-01 19:06:12 -0700

    Delete AK EF GUI directory

--- AK EF GUI/CONTRIBUTING.md
+++
@@ -1,9 +0,0 @@
-# Contributing
-
-- Fork the repo
-- Create a feature branch
-- Submit a PR
-- Follow code style
-
-No exploit code will be accepted.
-
--- AK EF GUI/loader/lua_sandbox.py
+++
@@ -1,9 +0,0 @@
-from lupa import LuaRuntime
-
-def run_lua_script(script_path):
-    lua = LuaRuntime(unpack_returned_tuples=True)
-
-    lua.globals().print = print
-
-    with open(script_path, "r", encoding="utf-8") as f:
-        lua.execute(f.read())
